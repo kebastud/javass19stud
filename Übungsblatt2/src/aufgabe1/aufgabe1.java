853c42e79e9b79a2aaac2a424469f69cb8b7e185
studkeba <kevin.bauer@stud.th-deg.de>
2019-06-20 12:30:34 +0200

    a1

--- Übungsblatt2/src/aufgabe1/aufgabe1.java
+++ Übungsblatt2/src/aufgabe1/aufgabe1.java
@@ -1,4 +1,18 @@
 package aufgabe1;
 
 public class aufgabe1 {
+    public static void main(String[] args){
+        int z=9;
+        int s=9;
+        int inrkement;
+        for(int i=1;i<=z;i++){
+             inrkement = 5;
+             inrkement = inrkement * i;
+            for(int j=1;j<=s;j++){
+                inrkement += 5;
+                System.out.printf(  inrkement + " " );
+            }
+            System.out.printf(   "\n" );
+        }
+    }
 }
